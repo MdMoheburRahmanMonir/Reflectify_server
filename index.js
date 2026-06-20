927af3430975376fb17c84ad19be0fe4e91f4bda
mdmoheburrahmanmonir <mdmohiburrahmanmanik@gmail.com>
2026-06-21 00:52:25 +0600

    User lesson post delete update api implemented

--- index.js
+++ index.js
@@ -31,17 +31,24 @@ async function run() {
 		const db = client.db('jobportal');
 		const usersCollection = db.collection('user');
 		const subscriptionCollection = db.collection('subscription');
+		const addLessonCollection = db.collection('add_lesson');
 
+
+		// Other Collection Data for showing purpose 
 		app.get('/api/jobs', async (req, res) => {
 			const jobsData = await collection.find().toArray();
 			res.send(jobsData);
 		})
+
+
+
+
+
 		// Subscription update route
 		app.post('/api/subscription/update/:id', async (req, res) => {
 			try {
 				const userId = req.params.id;
 				const { plan, id, userEmail } = req.body;
-
 				if (!plan) {
 					return res.status(400).json({ error: 'Plan is required' });
 				}
@@ -52,6 +59,107 @@ async function run() {
 				await subscriptionCollection.insertOne({ plan, id, userEmail, createdAt: new Date() })
 
 				return res.send({ success: true, message: "Subscription updated successfully" })
+
+			} catch (error) {
+				console.error(error);
+				return res.status(500).send({ error: "Server error" });
+			}
+		})
+
+
+
+
+
+
+
+
+
+
+		// User Add Lessons route api
+		app.post('/api/user/dashboard/add-lesson/:id', async (req, res) => {
+			try {
+				const id = req.params.id;
+				const data = req.body
+				data.createdTime = new Date();
+				const user = await usersCollection.findOne({
+					_id: new ObjectId(id)
+				})
+
+				if (!user) {
+					return res.status(403).send({ error: "Unauthorized status code" })
+				}
+				if (user.plan !== req.body.accessLevel) {
+					return res.status(403).send({ error: "Your Plan is free" })
+				}
+
+				const lesson = await addLessonCollection.insertOne(data)
+				res.send(lesson)
+			} catch (error) {
+				console.error(error);
+				return res.status(500).send({ error: "Server error" });
+			}
+		})
+
+		// User Fetch Lessons route api
+		app.get('/api/user/dashboard/my-lessons/:id', async (req, res) => {
+			try {
+				const id = req.params.id;
+				const user = await usersCollection.findOne({
+					_id: new ObjectId(id)
+				})
+
+				if (!user) {
+					return res.status(403).send({ error: "Unauthorized status code" })
+				}
+
+				const lessons = await addLessonCollection
+					.find({ userId: id })
+					.toArray();
+
+				res.send(lessons)
+			} catch (error) {
+				console.error(error);
+				return res.status(500).send({ error: "Server error" });
+			}
+		})
+
+		// User Fetch Lesson Delete api
+		app.delete('/api/user/dashboard/delete-lesson/:id', async (req, res) => {
+			try {
+				const id = req.params.id;
+				const bodyData = req.body;
+				const isMatchLessonId = await addLessonCollection.findOne({ _id: new ObjectId(id) })
+				if (!isMatchLessonId) {
+					res.status(403).send({ error: "Something wrong with your Data!" })
+				}
+				const isMatchUserId = await usersCollection.findOne({ _id: new ObjectId(bodyData.userId) })
+				if (isMatchUserId) {
+					console.log('User true');
+				}
+				const data = await addLessonCollection.deleteOne({ _id: new ObjectId(id) })
+				res.send(data)
+			} catch (error) {
+				console.error(error);
+				return res.status(500).send({ error: "Server error" });
+			}
+		})
+
+		// User Fetch Lesson Update api
+		app.patch('/api/user/dashboard/update-lesson/:id', async (req, res) => {
+			try {
+				const id = req.params.id;
+				const bodyData = req.body;
+				const isMatchLessonId = await addLessonCollection.findOne({ _id: new ObjectId(id) })
+				if (!isMatchLessonId) {
+					res.status(403).send({ error: "Something wrong with your Data!" })
+				}
+				const isMatchUserId = await usersCollection.findOne({ _id: new ObjectId(bodyData.userId) })
+				if (isMatchUserId) {
+					console.log('User true');
+				}
+
+				const data = await addLessonCollection.updateOne({ _id: new ObjectId(id) }, { $set: bodyData })
+				res.send(data)
 
 
 			} catch (error) {
@@ -65,8 +173,19 @@ async function run() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 		app.use((req, res) => {
-			res.send('Hello world')
+			res.send('You are searching invalid route.')
 		})
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
